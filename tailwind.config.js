0845057de3af91227034bac3c02f812a32205d56
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2023-07-09 01:57:25 +0200

    Add time and score widget

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["'Source Sans 3'", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
